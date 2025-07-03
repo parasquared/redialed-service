097f97de1a1cc5e8b4e769ec4dd4e7794758e50b
parasquared <bridget6454@gmail.com>
2025-07-02 18:25:53 -0700

    add a ton of client commands

--- ServiceVault/wtv-client/client.js
+++ ServiceVault/wtv-client/client.js
@@ -6,7 +6,7 @@ Content-Type: text/html`;
 
 data = `<html>
 <title>Client Commands</title>
-<!-- everything here was tested on a Philips Old Plus unit, running client 2.5.5 Internal / build 7352. information about commands may be incorrect/irrelevant on other hardware models/client builds -->
+<!-- most of these were tested on a Philips Old Plus MAT972, running client 2.5 Internal / build 7183. information about commands may be incorrect/irrelevant on other hardware models/client builds -->
 
 <a href="client:BottomOfPage">client:BottomOfPage</a>
 <br>
@@ -32,26 +32,49 @@ data = `<html>
 <a href="wtv-client:/explain">client:GoToExplanationPage (to do: figure out)</a>
 <br>
 
-<a href="client:GoToInfoPage">client:GoToInfoPage (the page that 411 poweroff code shows)</a>
+<a href="client:GoToInfoPage">client:GoToInfoPage (411 poweroff code page)</a>
 <br>
 
 <a href="client:showalert?message=This will hang up the phone, will not show the Reconnect dialog, and disables most of the shortcut keys.<br><br>Use activ or BeginDialing at the top of the page to reconnect.&buttonLabel1=Cancel&buttonAction1=client:donothing&buttonLabel2=Okay&buttonAction2=client:HangUpPhone">client:HangUpPhone (!)</a>
+<br>
+
+<a href="client:LogOnToPage">client:LogOnToPage (shows connecting screen, can back out)</a>
+<br>
+
+<a href="client:LogoShown">client:LogoShown (shows connecting screen, can back out)</a>
+<br>
+
+<a href="client:OpenAddressPanel">client:OpenAddressPanel (does nothing?)</a>
 <br>
 
 client:OpenKeyboard (only works when focused on text box)
 <br>
 
 <a href="client:showalert?message=This will turn off your box, similar to PowerOff.&buttonLabel1=Cancel&buttonAction1=client:donothing&buttonLabel2=Hit me!&buttonAction2=client:PowerOn">client:PowerOn (!)</a>
-
 <br>
+
 <a href="client:showalert?message=This will turn off your box, as if you pressed the power button and pressed Power off.&buttonLabel1=Cancel&buttonAction1=client:donothing&buttonLabel2=Hit me!&buttonAction2=client:PowerOff">client:PowerOff (!)</a>
+<br>
+
+<a href="client:showalert?message=This will turn off your box and put you into MiniBrowser once rebooted (if you're not running an Old Classic).&buttonLabel1=Cancel&buttonAction1=client:donothing&buttonLabel2=Hit me!&buttonAction2=client:PowerOff?miniBrowser&autoPowerOn">client:PowerOff?miniBrowser&autoPowerOn (!)</a>
+<br>
+
+<a href="client:ReloadPage">client:ReloadPage</a>
+<br>
+
+<a href="client:showalert?message=This will initiate a relog and show the user selector screen (if applicable), similar to selecting Switch User on the home screen. &buttonLabel1=Cancel&buttonAction1=client:donothing&buttonLabel2=Sounds cool&buttonAction2=client:relog">client:relog (!)</a>
 <br>
 
 <!-- not our problem if we get customer support emails about this. i told them not to do it officer -->
 <a href="client:showalert?message=THIS WILL CLEAR YOUR TERMINAL'S NVRAM (which includes some settings and Tellyscript).<br><br>Only use this if you are ABSOLUTELY sure what that means and you want to do it.&buttonLabel1=Cancel&buttonAction1=client:donothing&buttonLabel2=ASS BLAAAAAAAST!!&buttonAction2=client:ResetNVAndPowerOff">client:ResetNVAndPowerOff (!!!)</a>
 <br>
 
-<a href="client:SaveToFavorite">client:SaveToFavorite (shows animation but does nothing; missing arguments?)</a>
+<!-- missing args to actually save, not sure what they are however the expected output of saving a favorite normally is like this:
+wtv-favorite:/add?favorite-category=Personal&favorite-title=[page name]&favorite-thumbnail-type=image/wtv-bitmap&favorite-url=[url]
+with an image encoded in the POST data (otherwise defaults to canned/favorite_default.gif)
+putting the URL in the Go to panel causes it to give an error, but still saves properly.
+-->
+<a href="client:SaveToFavorite">client:SaveToFavorite (shows animation but does nothing)</a>
 <br>
 
 <a href="client:ShowCrashLog">client:ShowCrashLog (only works on internal builds?)</a>
@@ -65,6 +88,30 @@ client:OpenKeyboard (only works when focused on text box)
 <br>
 
 <a href="client:ShowServices">client:ShowServices</a>
+<br>
+
+<a href="client:ShowTVCrossoverRecent">client:ShowTVCrossoverRecent (TV home, press View after clicking)
+<br>
+
+<a href="client:ShowTVFullScreen">client:ShowTVFullScreen (RF input, press View after clicking)</a>
+<br>
+
+<a href="client:ShowTVFullScreen?UseVideoIn">client:ShowTVFullScreen?UseVideoIn (composite input, press View after clicking)</a>
+<br>
+
+<a href="client:ShowTVHome">client:ShowTVHome (press View after clicking)</a>
+<br>
+
+<a href="client:soundcapture?notify=client:donothing&device=audio&rate=8000&name=cache%3Avoicemail.wav&donebuttonlabel=Add%20to%20Message&open">client:SoundCapture (shows dialog, submitting does nothing)</a>
+<br>
+
+<a href="client:TogglePIP">client:TogglePIP (does nothing?)</a>
+<br>
+
+<a href="client:TriggerFlashDownload">client:TriggerFlashDownload (triggers update, only on Old Classic hardware)
+<br>
+
+<a href="client:UpdateFlash">client:UpdateFlash (triggers update, only on Old Classic hardware, may also reboot others)</a>
 <br>
 
 <!-- on viewer, it gives the dialing screen and after doing client:activ it hangs on a black screen -->
